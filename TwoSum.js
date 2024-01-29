a45ce3a203c82df65b434eb8cac4147edd6522b2
NsimDaghash <89819123+NsimDaghash@users.noreply.github.com>
2024-01-29 23:49:59 +0200

    Two Sum Problem
    Write a function that takes an array of integers and a target sum. The function should return indices of the two numbers such that they add up to the target.

--- TwoSum.js
+++ TwoSum.js
@@ -1,23 +1,31 @@
 /*
+Two Sum Problem:
+Write a function that takes an array of integers and a target sum. The function should return indices of the two numbers such that they add up to the target.
 
-Two Sum
-
-Write a function that takes an array of numbers (integers for the tests) and a target number. 
-It should find two different items in the array that, when added together, give the target value. 
-The indices of these items should then be returned in a tuple / list (depending on your language) like so: (index1, index2).
-
-For the purposes of this kata, some tests may have multiple answers; any valid solutions will be accepted.
-
-The input will always be valid (numbers will be an array of length 2 or greater, and all of the items will be numbers; 
-target will always be the sum of two different items from that array).
 */
 
-function twoSum(numbers, target) {
-  for (let i = 0; i < numbers.length - 1; i++) {
-    for (let j = i + 1; j < numbers.length; j++) {
-      if (numbers[i] + numbers[j] === target) {
-        return [i, j];
+function twoSum(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    const currentNum = nums[i];
+    const complement = target - currentNum;
+
+    if (nums.includes(complement)) {
+      for (let j = i + 1; j < nums.length; j++) {
+        if (complement == nums[j]) {
+          index = j;
+          return [i, index];
+        }
       }
     }
+
+    numIndices[currentNum] = i;
   }
+
+  // If no such pair is found, return null or an appropriate indicator
+  return null;
 }
+
+const nums = [5, 8, 9, 13];
+const target = 14;
+const result = twoSum(nums, target);
+console.log(result);
