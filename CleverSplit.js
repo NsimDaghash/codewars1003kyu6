9baa2987794d3d84d0655ff1cf95dd88b84bce6e
NsimDaghash <89819123+NsimDaghash@users.noreply.github.com>
2024-02-20 23:34:07 +0200

    Clever split
    You have a string. The string consists of words. Before words can be an exclamation mark !. Also some words are marked as one set with square brackets []. 
    You task is to split the string into separate words and sets.
     The set can't be contained in another set.
    
    Input:
     String with words (separated by spaces), ! and [].
    
    Output:
     Array with separated words and sets.

---
+++ CleverSplit.js
@@ -0,0 +1,31 @@
+/*
+
+Clever split
+
+You have a string. The string consists of words. Before words can be an exclamation mark !. Also some words are marked as one set with square brackets []. 
+You task is to split the string into separate words and sets.
+ The set can't be contained in another set.
+
+Input:
+ String with words (separated by spaces), ! and [].
+
+Output:
+ Array with separated words and sets.
+
+Examples:
+
+('Buy a !car [!red green !white] [cheap or !new]')  =>  ['Buy', 'a', '!car', '[!red green !white]', '[cheap or !new]']
+('!Learning !javascript is [a joy]')                =>  ['!Learning', '!javascript', 'is', '[a joy]']
+('[Cats and dogs] are !beautiful and [cute]')       =>  ['[Cats and dogs]', 'are', '!beautiful', 'and', '[cute]']
+
+*/
+
+function cleverSplit(s) {
+  // Regular expression to match words, sets within square brackets, and exclamation marks
+  const regex = /(\[.*?\]|\S+|!)/g;
+
+  // Use the regular expression to split the string
+  const result = s.match(regex);
+
+  return result || [];
+}
