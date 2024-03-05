50c611ce1c19f4dd9cd7ecfa434613f629d19bc6
NsimDaghash <89819123+NsimDaghash@users.noreply.github.com>
2024-03-05 23:22:41 +0200

    Adjacent repeated words in a string
    You know how sometimes you write the the same word twice in a sentence, but then don't notice that it happened? For example, you've been distracted for a second. 
    Did you notice that "the" is doubled in the first sentence of this description?
    As as aS you can see, it's not easy to spot those errors, especially if words differ in case, like "as" at the beginning of this sentence.
    Write a function that counts the number of sections repeating the same word (case insensitive). 
    The occurence of two or more equal words next after each other counts as one.

---
+++ AdjacentRepeatedWordsInString.js
@@ -0,0 +1,43 @@
+/*
+Adjacent repeated words in a string
+
+You know how sometimes you write the the same word twice in a sentence, but then don't notice that it happened? For example, you've been distracted for a second. 
+Did you notice that "the" is doubled in the first sentence of this description?
+
+As as aS you can see, it's not easy to spot those errors, especially if words differ in case, like "as" at the beginning of this sentence.
+
+Write a function that counts the number of sections repeating the same word (case insensitive). 
+The occurence of two or more equal words next after each other counts as one.
+
+Examples
+"dog cat"                  -->  0
+"dog DOG cat"              -->  1
+"apple dog cat"            -->  0
+"pineapple apple dog cat"  -->  0
+"apple apple dog cat"      -->  1
+"apple dog apple dog cat"  -->  0
+"dog dog DOG dog dog dog"  -->  1
+"dog dog dog dog cat cat"  -->  2
+"cat cat dog dog cat cat"  -->  3
+
+*/
+
+function countAdjacentPairs(searchString) {
+  searchString = searchString.toLowerCase().split(" ");
+  let inSection = false;
+  let ret = 0;
+
+  for (let i = 0; i < searchString.length - 1; i++) {
+    const firstWord = searchString[i];
+    const secondWord = searchString[i + 1];
+
+    if (firstWord !== secondWord) {
+      inSection = false;
+    } else if (firstWord === secondWord && !inSection) {
+      inSection = true;
+      ret += 1;
+    }
+  }
+
+  return ret;
+}
